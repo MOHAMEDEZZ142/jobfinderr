bb3eb1ce8975a3e28094d659c22ed15e38271958
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-03-01 08:07:44 +0200

    first commit

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-export const sequelize= new Sequelize("jobfinder1","root","root", {
-    host: 'localhost',
+export const sequelize= new Sequelize("jobfinder1","root","root",{ //"sql6687811","sql6687811","G8iewytUxy",  
+    host: 'localhost', // 'sql6.freesqldatabase.com', 
     dialect: 'mysql'
 });
 export const connectDB= async ()=>{
@@ -8,3 +8,11 @@ export const connectDB= async ()=>{
     .then(()=>{console.log("Database Connected")})
     .catch((err)=>{console.log("Connection field", err)})
 };
+
+/*
+Host: sql6.freesqldatabase.com
+Database name: sql6687811
+Database user: sql6687811
+Database password: G8iewytUxy
+Port number: 3306
+*/
