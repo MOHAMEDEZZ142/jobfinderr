36c4c9842720101562c58b71d06d78eea34e73ff
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-03-29 17:01:55 +0200

    updated1 commit

--- Src/modules/seeker/seeker.router.js
+++ Src/modules/seeker/seeker.router.js
@@ -4,6 +4,7 @@ import { asyncHandler } from "../../utels/asynHandler.js";
 import { isValid } from "../../middleware/validation.js";
 import { seekerSignUpSchema } from "./seeker.validation.js";
 import { isAuthenticatedSeeker } from "../../middleware/isAuthenticatedSeeker.js";
+import { uploadCloud } from "../../utels/multerCloud.js";
 const router= Router();
 
 router.post("/signup",isValid(seekerSignUpSchema),asyncHandler(signUp));
