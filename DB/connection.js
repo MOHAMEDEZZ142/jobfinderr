2f74fcdbdadb35cd8a3382b2019f090da3a74654
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-04-12 08:44:37 +0200

    updated1 commit

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-export const sequelize= new Sequelize("jobfinder1","root","root",{// "sql10698345","sql10698345","3NCvZL5nkD", 
-    host: 'localhost' ,//'sql10.freesqldatabase.com'
+export const sequelize= new Sequelize("jobfinder1","root","root",{ 
+    host: 'localhost' ,
     dialect: 'mysql'
 });
 export const connectDB= async ()=>{
@@ -8,9 +8,10 @@ export const connectDB= async ()=>{
     .then(()=>{console.log("Database Connected")})
     .catch((err)=>{console.log("Connection field", err)})
 };
-
+//'sql10.freesqldatabase.com'
 //
 /*
+// "sql10698345","sql10698345","3NCvZL5nkD",
 Host: sql10.freesqldatabase.com
 Database name: sql10698345
 Database user: sql10698345
