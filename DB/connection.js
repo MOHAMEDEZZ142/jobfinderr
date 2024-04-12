73d4d5c22f891e4d5ff08bdc4cd936d7e5224d7c
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-04-12 20:49:02 +0200

    updated1 commit

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-export const sequelize= new Sequelize("jobfinder1","root","root",{ 
-    host: 'localhost' ,
+export const sequelize= new Sequelize("sql10698345","sql10698345","3NCvZL5nkD",{ //"jobfinder1","root","root",
+    host: 'sql10.freesqldatabase.com', //'localhost' ,
     dialect: 'mysql'
 });
 export const connectDB= async ()=>{
@@ -8,13 +8,18 @@ export const connectDB= async ()=>{
     .then(()=>{console.log("Database Connected")})
     .catch((err)=>{console.log("Connection field", err)})
 };
-//'sql10.freesqldatabase.com'
-//
 /*
-// "sql10698345","sql10698345","3NCvZL5nkD",
 Host: sql10.freesqldatabase.com
 Database name: sql10698345
 Database user: sql10698345
 Database password: 3NCvZL5nkD
 Port number: 3306
+*/
+/*
+44.226.145.213
+54.187.200.255
+34.213.214.55
+35.164.95.156
+44.230.95.183
+44.229.200.200
 */
