63b72ee1ca0278e4f432fe27cac88d12c0e8098e
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-05-21 18:40:23 +0300

    updated1 commit

--- Src/modules/notification/notification.controller.js
+++ Src/modules/notification/notification.controller.js
@@ -1,8 +1,20 @@
+import { Job } from "../../../DB/models/Job.model.js";
+import { Comment } from "../../../DB/models/comment.model.js";
 import { Notification } from "../../../DB/models/notification.model.js";
+import { Post } from "../../../DB/models/post.model.js";
+import { Reaction } from "../../../DB/models/reaction.model.js";
+import { superUser } from "../../../DB/models/superUser.model.js";
 
 export const myNotification= async(req, res, next)=>{
     const notification= await Notification.findAll({
-        where:{receiverId:req.user.id}
+        where:{receiverId:req.user.id},
+        include:[
+            {model: superUser},
+            {model: Post},
+            {model: Job},
+            {model: Reaction},
+            {model: Comment},
+        ]
     })
     return res.json({success: true, notification});
 }
