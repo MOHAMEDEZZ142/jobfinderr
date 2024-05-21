212abb9f78a7020d4090b851941e51d052786c91
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-05-21 17:07:16 +0300

    updated1 commit

--- Src/modules/notification/notification.router.js
+++ Src/modules/notification/notification.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { isAuthenticated } from "../../middleware/isAuthenticated.js";
 import { myNotification } from "./notification.controller.js";
+import { asyncHandler } from "../../utels/asynHandler.js";
 const router= Router();
 router.get("/myNotification",isAuthenticated,asyncHandler(myNotification));
 export default router;
