91bd070f2e3f6e4a7e0e56940d542b2275dcb23c
MOHAMEDEZZ142 <m3zzforever14@gmail.com>
2024-05-27 20:36:14 +0300

    updated1 commit

--- Src/modules/job/Job.router.js
+++ Src/modules/job/Job.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addJob, deleteJob, deleteMyAllJobs, myAllJobs } from "./Job.controller.js";
+import { addJob, deleteJob } from "./Job.controller.js";
 import { asyncHandler } from "../../utels/asynHandler.js";
 import { isAuthenticatedCompany } from "../../middleware/isAuthenticatedCompany.js";
 import { isValid } from "../../middleware/validation.js";
@@ -7,8 +7,6 @@ import { addJobSchema, deleteJobSchema } from "./job.validation.js";
 const router= Router();
 
 router.post("/add",isAuthenticatedCompany,isValid(addJobSchema),asyncHandler(addJob));
-router.get("/myAlljobs",isAuthenticatedCompany,asyncHandler(myAllJobs));
 router.delete("/delete",isAuthenticatedCompany,isValid(deleteJobSchema), asyncHandler(deleteJob));
-router.delete("/deleteMyAlljobs",isAuthenticatedCompany, asyncHandler(deleteMyAllJobs));
 
 export default router;
